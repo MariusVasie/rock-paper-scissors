f2a58c928cc604d228d6103a43513479593391a2
MariusVasie <49002727+MariusVasie@users.noreply.github.com>
2024-04-29 07:23:10 +0300

    Start working on single round play

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /*
-Stage 1
+Stage 1 - Getting the computer choice
 When the page loads a random number will be generated between 1, 2 and 3
 Each number will be assigned to one of the elements
 1 = rock, 2 = paper and 3 = scissors
@@ -8,40 +8,60 @@ Log the random element that has been generated
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3)+1;
-    console.log('Random number is ',randomNumber);
     let computerChoice;
     if (randomNumber == 1) {
-        computerChoice = "Rock";
+        computerChoice = "rock";
     } else if (randomNumber == 2) {
-        computerChoice = "Paper";
+        computerChoice = "paper";
     } else {
-        computerChoice = "Scissors";
+        computerChoice = "scissors";
     }
     console.log('Comoputer choice is ', computerChoice);
   }
 getComputerChoice();
 
 /*
-Stage 2
+Stage 2 - Getting the human choice
 When the page loads a prompt will appear.
-The user is asked to choose between rock, paper, scissors
-He will have to enter 1 for rock, 2 for paper or 3 for scissors
+Ask the user to choose between rock, paper, scissors
 Log the option chosen by the user
 */
 
 function getHumanChoice() {
-    let promptValue = prompt("Choose between rock, paper and scissors. Type 1 for rock, 2 for paper, or 3 for scissors");
-    console.log("Promp value is ",promptValue);
-    let humanChoice;
-    if (promptValue === "1") {
-        humanChoice = "Rock";
-    } else if (promptValue === "2") {
-        humanChoice = "Paper";
-    } else if (promptValue === "3") {
-        humanChoice = "Scissors";
-    } else {
-        alert("Your input is not valid.");
-    }
+    let humanChoice = prompt("Choose between rock, paper and scissors");
     console.log('Human choice is ', humanChoice);
 }
 getHumanChoice();
+
+let humanScore = 0;
+let computerScore = 0;
+
+/*
+Stage 3 - Playing a single round
+Compare the computer and human values.
+Add a point to the winner
+*/
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == "rock") {
+        if (computerChoice == "paper") {
+            computerScore++;
+            console.log("Computer wins");
+        } else if (computerChoice == "scissors") {
+            humanScore++;
+            console.log("User wins");
+        } else {
+            console.log("It's a tie");
+        }
+    } else if (humanChoice == "paper") {
+        if (computerChoice == "rock") {
+            humanScore++;
+            console.log("User wins");
+        } else if (computerChoice == "scissors") {
+            computerScore++;
+            console.log("Computer wins");
+        } else {
+            console.log("It's a tie");
+        }
+    }
+  }
