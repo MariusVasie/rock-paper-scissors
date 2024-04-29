da177fd474241319e201383e77ff958a732612bd
MariusVasie <49002727+MariusVasie@users.noreply.github.com>
2024-04-29 06:58:31 +0300

    Add function for human choice

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ Log the random element that has been generated
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3)+1;
-    console.log(randomNumber)
+    console.log('Random number is ',randomNumber);
     let computerChoice;
     if (randomNumber == 1) {
         computerChoice = "Rock";
@@ -17,6 +17,31 @@ function getComputerChoice() {
     } else {
         computerChoice = "Scissors";
     }
-    console.log(computerChoice);
+    console.log('Comoputer choice is ', computerChoice);
   }
 getComputerChoice();
+
+/*
+Stage 2
+When the page loads a prompt will appear.
+The user is asked to choose between rock, paper, scissors
+He will have to enter 1 for rock, 2 for paper or 3 for scissors
+Log the option chosen by the user
+*/
+
+function getHumanChoice() {
+    let promptValue = prompt("Choose between rock, paper and scissors. Type 1 for rock, 2 for paper, or 3 for scissors");
+    console.log("Promp value is ",promptValue);
+    let humanChoice;
+    if (promptValue === "1") {
+        humanChoice = "Rock";
+    } else if (promptValue === "2") {
+        humanChoice = "Paper";
+    } else if (promptValue === "3") {
+        humanChoice = "Scissors";
+    } else {
+        alert("Your input is not valid.");
+    }
+    console.log('Human choice is ', humanChoice);
+}
+getHumanChoice();
