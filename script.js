7d8920ddab400922e61192dd6e8bce6c07fbf92d
MariusVasie <49002727+MariusVasie@users.noreply.github.com>
2024-08-02 06:53:14 +0300

    Fix the functionality of the buttons and prepare the page for results showcase

--- script.js
+++ script.js
@@ -29,14 +29,13 @@ Log the option chosen by the user
 
 function getHumanChoice(e) {
     // let promptValue = prompt("Choose between rock, paper and scissors");
-    console.log(e.target.innerText.toLowerCase());
-    // let humanChoice = e.target.value.toLowerCase();
-    // if (humanChoice == "rock" || humanChoice == "paper" || humanChoice == "scissors")  {
-    //     console.log('Your choice is ', humanChoice);
-    // } else {
-    //     alert("Your choice is not valid");
-    // }
-    // return humanChoice;
+    let humanChoice = e.target.innerText.toLowerCase();
+    if (humanChoice == "rock" || humanChoice == "paper" || humanChoice == "scissors")  {
+        console.log('Your choice is', humanChoice);
+    } else {
+        alert("Your choice is not valid");
+    }
+    return humanChoice;
 }
 
 /*
@@ -45,9 +44,12 @@ Compare the computer and human values.
 Add a point to the winner
 */
 
-function playRound(humanChoice, computerChoice) {
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(e) {
+    let humanChoice = getHumanChoice(e);
+    let computerChoice = getComputerChoice();
     if (humanChoice == "rock") {
         if (computerChoice == "paper") {
             computerScore++;
@@ -84,16 +86,16 @@ function playRound(humanChoice, computerChoice) {
     console.log("Your score:", humanScore, "Computer's score:", computerScore);
   }
 
-
-
-
   
-  /* Add event listeners */
+  /* Page elements and event listeners */
   const rockBtn = document.getElementById("rock");
   const paperBtn = document.getElementById("paper");
   const scissorsBtn = document.getElementById("scissors");
-  rockBtn.addEventListener("click", getHumanChoice);
-  paperBtn.addEventListener("click", getHumanChoice);
-  scissorsBtn.addEventListener("click", getHumanChoice);
+  rockBtn.addEventListener("click", playRound);
+  paperBtn.addEventListener("click", playRound);
+  scissorsBtn.addEventListener("click", playRound);
+
+  const humanResult = document.getElementById("human-result");
+  const computerResult = document.getElementById("computer-result");
 
 
